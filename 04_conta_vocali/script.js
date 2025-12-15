6c61a46a59d6bb969a886ee230c8694ada96224a
fabiogerritferrari <fabiogerritferrari@gmail.com>
2025-12-15 12:04:16 +0100

    creata una funzione che vada a contare le vocali minuscole e maiuscole in una frase e ritorni il numero di vocali presenti

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,33 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+// function contaVocali(nome){
+//     let contatore = 0;
+    
+//     for (let index = 0; index < nome.length; index++) {
+//         if(nome[index] === "a" || nome[index] === "e" || nome[index] === "i" || nome[index] === "o" || nome[index] === "u" || nome[index] === "A" || nome[index] === "E" || nome[index] === "I" || nome[index] === "O" || nome[index] === "U" ){
+//             contatore += 1;
+//         }
+        
+//     }
+//     return contatore
+// }
+
+
+const contaVocali = (nome) => {
+    let contatore = 0;
+    
+    for (let index = 0; index < nome.length; index++) {
+        if(nome[index] === "a" || nome[index] === "e" || nome[index] === "i" || nome[index] === "o" || nome[index] === "u" || nome[index] === "A" || nome[index] === "E" || nome[index] === "I" || nome[index] === "O" || nome[index] === "U" ){
+            contatore += 1;
+        }
+        
+    }
+    return contatore
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(contaVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
