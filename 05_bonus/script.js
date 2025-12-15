554038c487a96c62548dc7834601533578bf40ff
fabiogerritferrari <fabiogerritferrari@gmail.com>
2025-12-15 12:17:41 +0100

    creata funzione che crea un saluto personalizzato inserendo l'ora e il nome

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,42 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nomeUtente = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+// function salutoEducato(nome , ora){
+
+//     let saluto;
+
+//     if(ora < 13){
+//         saluto = "buongiorno" + " " + nome;
+//     }else if (ora > 13 && ora < 17){
+//         saluto = "buon pomeriggio" + " " + nome;
+//     }else{
+//         saluto = "buonasera" + " " + nome;
+//     }
+//     return saluto
+// }
+
+const salutoEducato = (nome , ora) => {
+
+    let saluto;
+
+    if(ora < 13){
+        saluto = "buongiorno" + " " + nome;
+    }else if (ora > 13 && ora < 17){
+        saluto = "buon pomeriggio" + " " + nome;
+    }else{
+        saluto = "buonasera" + " " + nome;
+    }
+    return saluto
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(salutoEducato(nomeUtente , 15));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
