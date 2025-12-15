95af797937f24e9754894952c8139688990da976
fabiogerritferrari <fabiogerritferrari@gmail.com>
2025-12-15 11:54:02 +0100

    creata funzione che controlla ogni parola all'interno dell'array e ritorna solo le parole contenenti la lettera selezionata, creata funzione anche in arrow

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,31 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+// function selezionaSoloLettera (array , lettera){
+//     let arrayLettera = [];
+
+//     for (let index = 0; index < array.length; index++) {
+//             if(array[index].includes(lettera) || array[index].includes(lettera.toUpperCase())){
+//                 arrayLettera.push(array[index]);
+//             } 
+//         }
+//     return arrayLettera
+// }
+
+const selezionaSoloLettera = (array , lettera) => {
+    let arrayLettera = [];
+
+    for (let index = 0; index < array.length; index++) {
+            if(array[index].includes(lettera) || array[index].includes(lettera.toUpperCase())){
+                arrayLettera.push(array[index]);
+            } 
+        }
+    return arrayLettera
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(selezionaSoloLettera (names , "l"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
